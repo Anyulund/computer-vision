d8209dc3f0620c5306c88f4c3d4ef40cd3a2a135
Anna Nyulund <anyulund@gmail.com>
2020-05-04 12:11:00 -0500

    work on clipping

--- Computer Vision I/basic_math_operations_practice.py
+++ Computer Vision I/basic_math_operations_practice.py
@@ -57,8 +57,53 @@ plt.show()
 '''
 #---------------------Brightness Enhancement------------------------------------
 brightnessOffset = 50
-
+'''
 # Add the offset for increasing brightness
 brightHigh = image + brightnessOffset
 
 # Display the outputs
+plt.figure(figsize=[20,20])
+plt.subplot(121);plt.imshow(image[...,::-1]);plt.title("Original Image");
+plt.subplot(122);plt.imshow(brightHigh[...,::-1]);plt.title("High Brightness");
+plt.show();
+
+print("Original Image Datatype : {}".format(image.dtype))
+print("Brightness Image Datatype : {}".format(brightHigh.dtype))
+
+print("Original Image Highest Pixel Intensity : {}".format(image.max()))
+print("Brightness Image Highest Pixel Intensity : {}".format(brightHigh.max()))
+
+a = np.array([[110,100],[120,130]],dtype='uint8')
+print(a)
+
+# Add 130 so that the last element encounters overflow
+# print(a+130)
+print(a-130)
+print(a+(-130))
+
+# Due to overflow and underflow problems, use OpenCV instead of Numpy to do mathematical operands
+print(cv2.add(a,130))   #Clipping
+
+# Convert to int32/int64 then clip
+a_int32 = np.int32(a)
+b = a_int32+130
+print(b)
+
+print(b.clip(0,255))
+b_uint8 = np.uint8(b.clip(0,255))
+
+#Convert to normalized float32/float64
+a_float32 = np.float32(a)/255
+b = a_float32 + 130/255
+print(b)
+
+c = b*255
+print("Output = \n{}".format(c))
+print("Clipped output= \n{}".format(c.clip(0,255)))
+b_uint8 = np.uint8(c.clip(0,255))
+print("uint8 output() = \n{}".format(b_uint8)
+'''
+#-----------------------Final Solution-----------------------------------------
+
+# Add the offset for increasing brightness
+brightHighOpenCV = cv2.add(image, np.ones(image.shape, dtype = 'uint8'))*brightnessOffset)
