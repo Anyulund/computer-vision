4970342cb5e0a269ddeab5db385b44720fa8da29
Anna Nyulund <anyulund@gmail.com>
2020-06-14 00:17:09 -0500

    Resumed and finished basic math operations on images

--- Computer Vision I/basic_math_operations_practice.py
+++ Computer Vision I/basic_math_operations_practice.py
@@ -105,5 +105,30 @@ print("uint8 output() = \n{}".format(b_uint8)
 '''
 #-----------------------Final Solution-----------------------------------------
 
+brightnessOffset = 50
+
 # Add the offset for increasing brightness
-brightHighOpenCV = cv2.add(image, np.ones(image.shape, dtype = 'uint8'))*brightnessOffset)
+brightHighOpenCV = cv2.add(image, np.ones(image.shape,dtype='uint8')*brightnessOffset)
+
+brightHighInt32 = np.int32(image) + brightnessOffset
+brightHighInt32Clipped = np.clip(brightHighInt32,0,255)
+
+# Display the outputs
+plt.figure(figsize=[20,20])
+plt.subplot(131);plt.imshow(image[...,::-1]);plt.title("original Image");
+plt.subplot(132);plt.imshow(brightHighOpenCV[...,::-1]);plt.title("Using cv2.add function");
+plt.subplot(133);plt.imshow(brightHighInt32Clipped[...,::-1]);plt.title("Using numpy and clipping");
+plt.show()
+
+# Add the offset for increasing brightness
+brightHighFloat32 = np.float32(image) + brightnessOffset
+brightHighFloat32NormalizedClipped = np.clip(brightHighFloat32/255,0,1)
+
+brightHighFloat32ClippedUint8 = np.uint8(brightHighFloat32NormalizedClipped*255)
+
+# Display the outputs
+plt.figure(figsize=[20,20])
+plt.subplot(131);plt.imshow(image[...,::-1]);plt.title("original Image");
+plt.subplot(132);plt.imshow(brightHighFloat32NormalizedClipped[...,::-1]);plt.title("Using np.float32 and clipping");
+plt.subplot(133);plt.imshow(brightHighFloat32ClippedUint8[...,::-1]);plt.title("Using int->float->int and clipping");
+plt.show()
